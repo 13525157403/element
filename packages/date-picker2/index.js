368564ab0417251409c05c307f42f75ef05b768d
chang <1375843267@qq.com>
2022-02-23 10:24:20 +0800

    pick-date

---
+++ packages/date-picker2/index.js
@@ -0,0 +1,8 @@
+import DatePicker2 from './src/picker/date-picker';
+
+/* istanbul ignore next */
+DatePicker2.install = function install(Vue) {
+  Vue.component(DatePicker2.name, DatePicker2);
+};
+
+export default DatePicker2;
